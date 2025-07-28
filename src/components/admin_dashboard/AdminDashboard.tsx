9ec56ac69497d7089d4373d4e59f3cf18d65732e
Marlon <marlonhoffmann001@gmail.com>
2025-07-28 20:32:54 -0300

    iniciando layout da página de administrador

--- src/components/admin_dashboard/AdminDashboard.tsx
+++ src/components/admin_dashboard/AdminDashboard.tsx
@@ -1,5 +1,24 @@
+import "./AdminDashboard.css"
+import { Link } from "react-router-dom"
 export function AdminDashboard(){
     return(
-        <h1>Pagina de admin</h1>
+        <main>
+            <header>
+
+            </header>
+            <section id="optionsAdmin">
+                <Link to="/products" className="Link">Produtos</Link>
+                <Link to="/users" className="Link">Usuários</Link>
+                <Link to="/supplier" className="Link">Fornecedores</Link>
+                <Link to="/general" className="Link">Informções Gerais</Link>
+            </section>
+            <section id="mainSection">
+
+            </section>
+            <footer>
+                <p>Conteúdo para fins didáticos.</p>
+                <p>Desenvolvido por Marlon Hoffmann.</p>
+            </footer>
+        </main>
     )
 }
