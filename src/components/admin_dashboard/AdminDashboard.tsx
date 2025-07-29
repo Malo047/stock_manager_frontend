2a99fd03965b4283e52735bb4714b82c91026dda
Marlon <marlonhoffmann001@gmail.com>
2025-07-28 21:39:11 -0300

    Update
    Refatorado pagina de admin, pois gerou alguns bugs em outras páginas, e corrigido tamanho do input no login.

--- src/components/admin_dashboard/AdminDashboard.tsx
+++ src/components/admin_dashboard/AdminDashboard.tsx
@@ -2,9 +2,17 @@ import "./AdminDashboard.css"
 import { Link } from "react-router-dom"
 export function AdminDashboard(){
     return(
-        <main>
-            <header>
+        <main className="admin-main">
+            <header id="admin-header">
+                <div id="admin-logo">
 
+                </div>
+                <div id="admin-title">
+                    
+                </div>
+                <div id="admin-profile">
+
+                </div>
             </header>
             <section id="optionsAdmin">
                 <Link to="/products" className="Link">Produtos</Link>
@@ -12,10 +20,10 @@ export function AdminDashboard(){
                 <Link to="/supplier" className="Link">Fornecedores</Link>
                 <Link to="/general" className="Link">Informções Gerais</Link>
             </section>
-            <section id="mainSection">
+            <section id="main-section-admin">
 
             </section>
-            <footer>
+            <footer id="footer-admin">
                 <p>Conteúdo para fins didáticos.</p>
                 <p>Desenvolvido por Marlon Hoffmann.</p>
             </footer>
