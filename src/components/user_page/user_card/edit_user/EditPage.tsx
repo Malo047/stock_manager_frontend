cb35298eef6fc0b4817fd7f2308432d36b657821
Marlon <marlonhoffmann001@gmail.com>
2025-08-07 12:49:34 -0300

    Iniciando estilização do edit user

--- src/components/user_page/user_card/edit_user/EditPage.tsx
+++ src/components/user_page/user_card/edit_user/EditPage.tsx
@@ -2,11 +2,18 @@ import "./EditPage.css"
 export function EditPage(){
     return(
         <div id="edit-page-main" className="exclude-overlay">
+            
             <form action="">
-                <label htmlFor="username-edit"></label>
-                <input type="text" id="username-edit" />
-                <label htmlFor="email-edit"></label>
-                <input type="email" id="email-edit"/>
+                <input type="text" id="username-edit" placeholder="Username"/><br />
+                <input type="email" id="email-edit" placeholder="E-mail"/><br />
+                <label htmlFor="user-level-edit">User level:</label>
+                <select name="user-level" id="user-level-edit">
+                    <option value="null">Selecione</option>
+                    <option value="1">Administrator</option>
+                    <option value="2">User</option>
+                </select><br />
+                <button>Cancelar</button>
+                <button type="submit">Editar</button>
             </form>
         </div>
     )
